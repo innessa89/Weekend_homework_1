43a08f62c1a59d51bfa37e769005754a2b03070f
innessa89 <innessa89@gmail.com>
2021-10-15 20:11:18 +0100

    Second commit

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -13,4 +13,34 @@ def get_pets_sold(pets):
 def increase_pets_sold(shop, pets):
     shop["admin"]["pets_sold"]+=pets
 
+def get_stock_count(stock):   
+    return len(stock["pets"])
+
+
+def get_pets_by_breed(shop, breed):
+    cats_list=[]
+    for cat in shop["pets"]:
+        if cat["breed"] == breed:
+            cats_list.append(cat)
+    return cats_list   
+
+def find_pet_by_name(shop,name):
+    for pet in shop["pets"]:
+        if pet["name"]==name:       
+           return pet        
+
+def remove_pet_by_name(shop,name):
+   for pet in shop["pets"]:
+        if pet["name"]==name:   
+            shop["pets"].remove(pet)
+            return
+
+
+       
+             
+
+
+    
+
+
 
